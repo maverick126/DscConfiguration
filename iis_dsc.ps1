dad1395618eb58d6194796b464f79943ba06f037
maverick126 <chitostodomingo@gmail.com>
2020-05-26 17:44:15 +1000

    second commit

---
+++ iis_dsc.ps1
@@ -0,0 +1,97 @@
+Configuration iis_dsc {
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'xWebAdministration'
+
+    Node localhost {
+
+        WindowsFeature IIS
+        {
+            Ensure = 'Present'
+            Name = 'Web-Server'
+        }
+        WindowsFeature IISConsole
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Console'
+            DependsOn = '[WindowsFeature]IIS'
+        }
+        WindowsFeature IISScriptingTools
+        {
+            Ensure = 'Present'
+            Name = 'Web-Scripting-Tools'
+            DependsOn = '[WindowsFeature]IIS'
+        }
+        WindowsFeature AspNet
+        {
+            Ensure = 'Present'
+            Name = 'Web-Asp-Net45'
+            DependsOn = @('[WindowsFeature]IIS')
+        }
+        WindowsFeature WCFService
+        {
+            Ensure = 'Present'
+            Name = 'NET-WCF-Services45'
+        }        
+        WindowsFeature HTTPActivation
+        {
+            Ensure = 'Present'
+            Name = 'NET-WCF-HTTP-Activation45'
+            DependsOn = @('[WindowsFeature]WCFService')
+        }           
+        WindowsFeature IdentityFoundation
+        {
+            Ensure = 'Present'
+            Name = 'Windows-Identity-Foundation'
+            DependsOn = @('[WindowsFeature]AspNet')
+        }        
+        xWebsite DefaultSite
+        {
+            Ensure = 'Present'
+            Name = 'Default Web Site'
+            State = 'Stopped'
+            PhysicalPath = 'C:\inetpub\wwwroot'
+            DependsOn = @('[WindowsFeature]IIS','[WindowsFeature]AspNet')
+        }
+        File demofolder
+        {
+            Ensure = 'Present'
+            Type = 'Directory'
+            DestinationPath = "C:\inetpub\wwwroot\demo"
+        }
+        File Indexfile
+        {
+            Ensure = 'Present'
+            Type = 'file'
+            DestinationPath = "C:\inetpub\wwwroot\demo\index.html"
+            Contents = "<html>
+        <header><title>This is Demo Website</title></header>
+        <body>
+        Welcome to the World!
+        </body>
+        </html>"
+        }
+        xWebAppPool DemoWebAppPool
+        {
+            Ensure = "Present"
+            State = "Started"
+            Name = "demo"
+        }
+        xWebsite DemoWebSite
+        {
+            Ensure = 'Present'
+            State = 'Started'
+            Name = "Demo"
+            PhysicalPath = "C:\inetpub\wwwroot\demo"
+        }
+
+        xWebApplication demoWebApplication
+        {
+            Name = "demo"
+            Website = "demo"
+            WebAppPool = "demo"
+            PhysicalPath = "C:\inetpub\wwwroot\demo"
+            Ensure = 'Present'
+            DependsOn = @('[xWebSite]DemoWebSite')
+        }
+    }
+}
